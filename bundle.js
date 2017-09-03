5f1c96e8e6eeb80261c2178cae2bb66c09763541
ISABELLE <isabelle.k.miller@gmail.com>
2017-09-03 09:58:19 -0700

    updated about section in bundle

--- bundle.js
+++ bundle.js
@@ -11614,7 +11614,7 @@ var Experience = function (_Component) {
         _react2.default.createElement(
           'div',
           { className: 'about-text' },
-          'Hi, my name is Isabelle. I like to code, travel, write, bake, paint, and think about how tech can be harnessed to improve governance, social equality, equity in education, and quality of life in the developing world. I prefer my coffee with milk and my cookies with chocolate.',
+          'Hi, my name is Isabelle. I like to code, travel, write, bake, paint, and think about how tech can be harnessed to improve governance, social equality, equity in education, and quality of life in the developing world. I also hold a Master of Public Policy from the Hertie School of Governance in Berlin and speak German, Spanish, Thai, and rusty Portuguese. I prefer my coffee with milk and my cookies with chocolate.',
           _react2.default.createElement('br', null),
           _react2.default.createElement('br', null),
           'To contact me directly, feel free to reach out at me @ isabellekmiller.com.'
