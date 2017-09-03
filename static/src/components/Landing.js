4038e4a9b08060b35caeb124cd4d1df23b98b8e7
ISABELLE <isabelle.k.miller@gmail.com>
2017-09-02 18:29:34 -0700

    added social media logos, some light CSSing

--- static/src/components/Landing.js
+++ static/src/components/Landing.js
@@ -3,6 +3,8 @@ require('../../stylesheets/app.scss')
 import React, { Component, PropTypes } from 'react';
 import LandingMenuContainer from '../containers/LandingMenuContainer';
 import LandingCarouselContainer from '../containers/LandingCarouselContainer';
+import Portfolio from './Portfolio';
+// import linkedin from '../../images/linkedin_logo.png'
 
 class Landing extends Component {
 
@@ -10,13 +12,18 @@ class Landing extends Component {
     return(
       <div className='landing-contents'>
         <div className='menu-container'>
-          <LandingMenuContainer menuItems={['ABOUT', 'PORTFOLIO', 'CV']} />
+          <LandingMenuContainer menuItems={['ABOUT', 'PORTFOLIO']} />
+          <div className='logo-container'>
+            <a href='https://github.com/sloloris' target='_blank'><img src='/images/github_logo.png' className='logo-img' /></a>
+            <a href='https://www.instagram.com/belleandcompass/' target='_blank'><img src='/images/instagram_logo.png' className='logo-img' /></a>
+            <a href='https://www.linkedin.com/in/isabelle-miller/' target='_blank'><img src='/images/linkedin_logo.png' className='logo-img'/></a>
+          </div>
         </div>
+
         <div className='carousel-container'>
           <LandingCarouselContainer 
-            viewItems={[<div className='carousel-item'>Isabelle Miller is a software engineer for hire based in SF, CA.</div>,
-                        <div className='carousel-item'>PORTFOLIO</div>, 
-                        <div className='carousel-item'>CV</div> ]} />
+            viewItems={[<div className='carousel-item'><p className='isabelle-miller'>Isabelle Miller</p> is a software engineer for hire based in SF, CA.</div>,
+                        <div className='carousel-item'><Portfolio /></div>, ]} />
         </div>
       </div>
     )
