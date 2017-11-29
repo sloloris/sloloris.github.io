0c23469ee4304a860e026e3e02fdd8484f678b07
ISABELLE <isabelle.k.miller@gmail.com>
2017-11-28 19:38:31 -0500

    updated bundle

--- bundle.js
+++ bundle.js
@@ -11589,24 +11589,18 @@ var Experience = function (_Component) {
             { className: 'name' },
             'Isabelle Miller'
           ),
-          ' is a software engineer ',
-          _react2.default.createElement(
-            'a',
-            { href: 'https://www.linkedin.com/in/isabelle-miller/', target: '_blank' },
-            'for hire'
-          ),
-          ' based in SF, CA.'
+          ' is a software engineer based in SF, CA.'
         ),
         _react2.default.createElement(
           'div',
           { className: 'about-text' },
-          'Hi, my name is Isabelle. I like to code, travel, write, bake, paint, sing (currently ',
+          'Hi, my name is Isabelle. I like to code, travel, write, bake, paint, ',
           _react2.default.createElement(
             'a',
             { href: 'http://www.sfchoral.org/site/', target: '_blank' },
-            'here'
+            'sing'
           ),
-          '), and think about how tech can be harnessed to improve governance, social equality, equity in education, and quality of life in the developing world. I also hold a Master of Public Policy from the Hertie School of Governance in Berlin and speak German, Spanish, Thai, and rusty Portuguese. I prefer my coffee with milk and my cookies with chocolate.',
+          ', and think about how tech can be harnessed to improve governance, social equality, equity in education, and quality of life in the developing world. I also hold a Master of Public Policy from the Hertie School of Governance in Berlin and speak German, Spanish, Thai, and rusty Portuguese. I prefer my coffee with milk and my cookies with chocolate.',
           _react2.default.createElement('br', null),
           _react2.default.createElement('br', null),
           'To contact me directly, feel free to reach out at me @ isabellekmiller.com.'
