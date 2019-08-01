008942e41ab67044acdbde6d4e6185bf971b3301
Isabelle Miller <isabelle@Isabelles-MacBook-Air.local>
2019-08-01 16:36:03 -0400

    update location

--- static/src/components/About.js
+++ static/src/components/About.js
@@ -3,16 +3,16 @@ require('../../stylesheets/about.scss')
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Experience extends Component {
+class About extends Component {
 
 
   render() {
     return(
       <div>
-        <div className="header-text"><p className='name'>Isabelle Miller</p> is a software engineer based in SF, CA.
+        <div className="header-text"><p className='name'>Isabelle Miller</p> is a software engineer & policy professional based in London, UK.
         </div>
         <div className='about-text'>
-          Hi, my name is Isabelle. I like to code, travel, write, bake, paint, <a href='http://www.sfchoral.org/site/' target='_blank'>sing</a>, and think about how tech can be harnessed to improve governance, social equality, equity in education, and quality of life in the developing world. I also hold a Master of Public Policy from the Hertie School of Governance in Berlin and speak German, Spanish, Thai, and rusty Portuguese. I prefer my coffee with milk and my cookies with chocolate.
+          Hi, my name is Isabelle. I like to code, travel, write, bake, sing, and think about how tech can be harnessed to improve governance, social equality, equity in education, and quality of life in the developing world. I also hold a Master of Public Policy from the Hertie School of Governance in Berlin and speak German, Spanish, Thai, and rusty Portuguese. I prefer my coffee with milk and my cookies with chocolate.
           <br />
           <br />
           To contact me directly, feel free to reach out at me @ isabellekmiller.com.
@@ -22,4 +22,4 @@ class Experience extends Component {
   }
 }
 
-export default Experience
+export default About
