55cdf1a0498d3d71f1964ede11d3ec4e2a033f6f
Isabelle Miller <isabelle@Isabelles-MacBook-Air.local>
2019-08-04 11:30:26 -0400

    update location to include Berlin

--- bundle.js
+++ bundle.js
@@ -11612,7 +11612,7 @@ var About = function (_Component) {
             { className: 'name' },
             'Isabelle Miller'
           ),
-          ' is a software engineer & policy professional based in London, UK.'
+          ' is a software engineer & policy professional based in London, UK.*'
         ),
         _react2.default.createElement(
           'div',
@@ -11620,7 +11620,10 @@ var About = function (_Component) {
           'Hi, my name is Isabelle. I like to code, travel, write, bake, sing, and think about how tech can be harnessed to help the earth and to improve governance, social equality, and equity in education. I also hold a Master of Public Policy from the Hertie School of Governance in Berlin and speak German, Spanish, Thai, and rusty Portuguese. I prefer my coffee with milk and my cookies with chocolate.',
           _react2.default.createElement('br', null),
           _react2.default.createElement('br', null),
-          'To contact me directly, feel free to reach out at isabelle [dot] k [dot] miller [at] gmail.com.'
+          'To contact me directly, feel free to reach out at isabelle [dot] k [dot] miller [at] gmail.com.',
+          _react2.default.createElement('br', null),
+          _react2.default.createElement('br', null),
+          '*As of August 2019, I am currently temporarily based in Berlin, Germany, for a fellowship ending in October. Starting in November, I will be seeking new opportunities in London.'
         )
       );
     }
